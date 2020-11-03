58b2ec32f96d58ecd914f72fbbc1a785257ac7cb
bhasher <bhasher.anonyme@gmail.com>
2020-11-03 19:14:56 +0100

    Default code and specific code access

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from api.api import api
+from db import db
+from api.data import DEBUG
 
 app = Flask(__name__, template_folder='templates')
 app.register_blueprint(api, url_prefix='/api')
+
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+
 @app.route('/<doc_id>')
 @app.route('/editor/<doc_id>')
 def editor(doc_id):
-    return render_template('editor.html')
+    doc = db.get_document(doc_id)
+    return render_template('editor.html', document=doc)
 
 
-app.run(debug=True)
+app.run(debug=DEBUG)
