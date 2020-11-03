c4e5469088e2da745a06693eb3b0d859bc8b786c
allEyezOnCode <alexandredewilde2001@gmail.com>
2020-11-03 19:53:09 +0100

    add create_document

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, redirect, abort
 from api.api import api
 from db import db
 from api.data import DEBUG
@@ -12,11 +12,25 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/create_document", methods=['POST'])
+def create_document():
+    try:
+        doc_id = db.create_document()
+        return redirect(f"/editor/{doc_id}")
+    except Exception:
+        return abort(500)
+
+
 @app.route('/<doc_id>')
 @app.route('/editor/<doc_id>')
 def editor(doc_id):
-    doc = db.get_document(doc_id)
-    return render_template('editor.html', document=doc)
+    try:
+        doc_id = db.get_document(doc_id)
+        if doc_id is not None:
+            return render_template('editor.html', document=doc_id)
+        return abort(404)
+    except Exception:
+        return abort(500)
 
 
 app.run(debug=DEBUG)
