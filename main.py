27e7c157e5c544a330317f60e20e68970af0eff0
allEyezOnCode <alexandredewilde2001@gmail.com>
2020-11-03 11:11:39 +0100

    small update

--- main.py
+++ main.py
@@ -16,21 +16,19 @@ def document(document_id):
 def create_document():
     doc_id = str(uuid.uuid4())
     db.create_document(doc_id)
-    return jsonify({"message": doc_id})
+    return jsonify({"doc_id": doc_id})
 
 @app.route('/upload', methods=['POST'])
 def upload():
     doc_id, doc_content = request.form['doc_id'], request.form['doc_content']
     db.update_document(doc_id, doc_content)
-    print(db.get_document(doc_id))
-    return jsonify({"message":"file_uploaded"})
+    return jsonify({"uploaded":True})
 
 
 @app.route('/download', methods=['POST'])
 def download():
     doc_id = request.form['doc_id']
     content = db.get_document(doc_id)
-    print(content)
     return jsonify({"content": content})
 
 app.run(debug=True)
