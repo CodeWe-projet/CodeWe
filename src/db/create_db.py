318f201dad8fd905e463edc10cd0e2eaf1ef5243
allEyezOnCode <alexandredewilde2001@gmail.com>
2020-11-03 15:12:46 +0100

    update

--- src/db/create_db.py
+++ src/db/create_db.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     conn = sqlite3.connect("documents.db")
     c = conn.cursor()
 
-    c.execute("CREATE TABLE documents (id INTEGER PRIMARY KEY AUTOINCREMENT, creation_date, last_seen, text)")
+    c.execute("CREATE TABLE documents (id INTEGER PRIMARY KEY AUTOINCREMENT, doc_id, creation_date, last_seen, text)")
 
     conn.commit()
     conn.close()
