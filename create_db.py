fa014c322b01bfbe0d94ad66ea83c7e97e25409d
allEyezOnCode <alexandredewilde2001@gmail.com>
2020-11-03 13:49:44 +0100

    add api

--- create_db.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("documents.db")
-c = conn.cursor()
-
-c.execute("CREATE TABLE documents (id, creation_date, last_seen, text)")
-
-conn.commit()
-conn.close()
--- src/main.py
+++ src/main.py
@@ -1,34 +1,16 @@
-import uuid
-import db
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template
+from api.api import api
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+app.register_blueprint(api, url_prefix='/api')
 
 @app.route('/')
 def home():
-    return "Welcome to CodeWe!"
+    return render_template("main.html")
 
-@app.route('/document/<document_id>')
-def document(document_id):
-    return f"Document{document_id}"
+@app.route('/editor')
+def editor():
+    return render_template('editor.html')
 
-@app.route('/create_document')
-def create_document():
-    doc_id = str(uuid.uuid4())
-    db.create_document(doc_id)
-    return jsonify({"doc_id": doc_id})
-
-@app.route('/upload', methods=['POST'])
-def upload():
-    doc_id, doc_content = request.form['doc_id'], request.form['doc_content']
-    db.update_document(doc_id, doc_content)
-    return jsonify({"uploaded":True})
-
-
-@app.route('/download', methods=['POST'])
-def download():
-    doc_id = request.form['doc_id']
-    content = db.get_document(doc_id)
-    return jsonify({"content": content})
 
 app.run(debug=True)
