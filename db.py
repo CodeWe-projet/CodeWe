1e034d6b35e8f3c13c2230a03ac8feb98b073b9a
allEyezOnCode <alexandredewilde2001@gmail.com>
2020-11-03 10:05:13 +0100

    update

--- db.py
+++ db.py
@@ -1,9 +1,43 @@
 import sqlite3
 
-#TODO create documents.db
+
 conn = sqlite3.connect('documents.db')
-#c = conn.cursor()
+c = conn.cursor()
 
 
+def get_document(doc_id):
+    """Get doc in the databse with id specified
+
+    Args:
+        doc_id (str): id of the doc to modify
+
+    Returns:
+        tuple: all values with the document
+    """
+    query = "SELECT * FROM documents WHERE id = ?"
+    c.execute(query, (doc_id,))
+    return c.fetchone()
+
+
+def update_document(doc_id, text_content):
+    """Update the document in the db with the id specified
+
+    Args:
+        doc_id (str): id of the document to change
+        text_content (str): the text to change
+    """
+    query = "UPDATE documents SET text = ? WHERE id = ?"
+    c.execute(query, (text_content, doc_id))
+    conn.commit()
+
+
+def create_document(doc_id):
+    """Create the document with id specified
+
+    Args:
+        doc_id (str): doc with id specifed
+    """
+    query = 'INSERT INTO documents (id) VALUES (?)'
+    c.execute(query, (doc_id,))
 
 
