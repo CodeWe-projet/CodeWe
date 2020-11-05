a466b08c9fb2327c0684b026d050b14f9f072c5a
TheoTechnicguy <19630890+TheoTechnicguy@users.noreply.github.com>
2020-11-05 15:16:47 +0100

    Concatenate everything
    Version 0.1.3
    - Add comments and section
    - Add stored procedures

--- db/sql_files/create_db.sql
+++ db/sql_files/create_db.sql
@@ -4,9 +4,10 @@
 -- Ext: sql
 -- Licensed under MIT
 -- Copyright (c) 2020 Alexandre Dewilde, Brieuc Dubois and Theo Technicguy.
--- Version 0.1.2
+-- Version 0.1.3
 -- -----------------------
 
+-- Database
 DROP DATABASE IF EXISTS `codewe`;
 CREATE DATABASE `codewe`;
 USE `codewe`;
@@ -14,6 +15,7 @@ USE `codewe`;
 SET NAMES utf8;
 SET character_set_client = utf8mb4;
 
+-- Tables
 CREATE TABLE `documents` (
   `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `document_id` CHAR(5) NOT NULL,
@@ -22,3 +24,42 @@ CREATE TABLE `documents` (
   `content` LONGTEXT,
   PRIMARY KEY (`id`, `document_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+-- Stored procedures
+DROP PROCEDURE IF EXISTS `get_document`;
+
+DELIMITER $$
+CREATE PROCEDURE get_document(param_document_id CHAR(5))
+BEGIN
+  SELECT *
+    FROM documents d
+    WHERE d.document_id = param_document_id;
+END$$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `update_document`;
+
+DELIMITER $$
+CREATE PROCEDURE update_document(
+  param_document_id CHAR(5),
+  param_content LONGTEXT
+)
+BEGIN
+  UPDATE documents d
+    SET content = param_content, last_viewed_date = NOW()
+    WHERE d.document_id = param_document_id;
+END$$
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS `create_document`;
+
+DELIMITER $$
+CREATE PROCEDURE create_document(
+  param_document_id CHAR(5),
+  param_content LONGTEXT
+)
+BEGIN
+  INSERT INTO documents(document_id, content)
+    VALUES (param_document_id, param_content);
+END$$
+DELIMITER ;
