7415131a0858a705f93c9a7167765dea1a1da558
bhasher <bhasher.anonyme@gmail.com>
2020-11-09 14:58:48 +0100

    Synthax highlighting

--- src/static/js/editor.js
+++ src/static/js/editor.js
@@ -177,23 +177,124 @@ keyup = (e => {
             let uuid = getRandomString(10);
             new_element.setAttribute('uuid', uuid);
 
-            requests = [
+            socket.send('update text', [
                 newElementRequest(uuid, previous_uuid),
                 updateElementRequest(previous_element),
                 updateElementRequest(new_element)
-            ]
-
-            socket.send('update text', requests);
+            ]);
 
             break
         default:
-            let el = get_uuid_element();
-            requests = [
+            const el = get_uuid_element();
+            socket.send('update text', [
                 updateElementRequest(el)
-            ]
-            socket.send('update text', requests)
+            ]);
     }
 });
 
 socket = new Socket(doc_id, receive);
 editor = new Editor('editor', 4, keyup, keydown);
+
+function rainbow(block, lang){
+    const prism = new Prism(patterns[lang].concat(patterns['generic']));
+    block.innerHTML = prism.refract(block.innerText);
+}
+
+for(const child of document.getElementById('editor').children){
+    rainbow(child, 'python');
+}
+
+/* https://jsfiddle.net/nrx9yvw9/5/ */
+function createRange(node, chars, range) {
+    if (!range) {
+        range = document.createRange()
+        range.selectNode(node);
+        range.setStart(node, 0);
+    }
+
+    if (chars.count === 0) {
+        range.setEnd(node, chars.count);
+    } else if (node && chars.count >0) {
+        if (node.nodeType === Node.TEXT_NODE) {
+            if (node.textContent.length < chars.count) {
+                chars.count -= node.textContent.length;
+            } else {
+                 range.setEnd(node, chars.count);
+                 chars.count = 0;
+            }
+        } else {
+            for (var lp = 0; lp < node.childNodes.length; lp++) {
+                range = createRange(node.childNodes[lp], chars, range);
+
+                if (chars.count === 0) {
+                   break;
+                }
+            }
+        }
+   }
+
+   return range;
+}
+
+function setCurrentCursorPosition(element, chars) {
+    if (chars >= 0) {
+        let selection = document.getSelection();
+
+        let range = createRange(element, { count: chars });
+
+        if (range) {
+            range.collapse(false);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    }
+}
+
+/* Based of https://stackoverflow.com/a/4812022/11247647 */
+function getCaretCharacterOffsetWithin(element) {
+    let caretOffset = 0;
+    let doc = element.ownerDocument || element.document;
+    let win = doc.defaultView || doc.parentWindow;
+    let sel;
+    if (typeof win.getSelection != "undefined") {
+        sel = win.getSelection();
+        if (sel.rangeCount > 0) {
+            let range = win.getSelection().getRangeAt(0);
+            let preCaretRange = range.cloneRange();
+            preCaretRange.selectNodeContents(element);
+            preCaretRange.setEnd(range.endContainer, range.endOffset);
+            caretOffset = preCaretRange.toString().length;
+        }
+    } else if ( (sel = doc.selection) && sel.type !== "Control") {
+        let textRange = sel.createRange();
+        let preCaretTextRange = doc.body.createTextRange();
+        preCaretTextRange.moveToElementText(element);
+        preCaretTextRange.setEndPoint("EndToEnd", textRange);
+        caretOffset = preCaretTextRange.text.length;
+    }
+    return caretOffset;
+}
+
+editor.editor.addEventListener('keyup', e => {
+
+    /*const sub_el = window.getSelection().getRangeAt(0);
+
+    let container = sub_el.startContainer;
+    let n = sub_el.startOffset;
+
+    console.log(container);
+    console.log(n);
+
+    while(container.nodeType === Node.TEXT_NODE || !container.hasAttribute('uuid')){
+        container = container.parentElement;
+        n += container.getBoundingClientRect().left || 0;
+        console.log(container);
+        console.log(n);
+    }*/
+
+    let offset = getCaretCharacterOffsetWithin(get_uuid_element());
+
+    rainbow(get_uuid_element(), 'python');
+
+    setCurrentCursorPosition(get_uuid_element(), offset);
+});
---
+++ src/static/js/patterns.js
@@ -0,0 +1,139 @@
+const patterns = {
+    generic: [
+        {
+            matches: {
+                1: [
+                    {
+                        name: 'keyword.operator',
+                        pattern: /\=|\+/g
+                    },
+                    {
+                        name: 'keyword.dot',
+                        pattern: /\./g
+                    }
+                ],
+                2: {
+                    name: 'string',
+                    matches: {
+                        name: 'constant.character.escape',
+                        pattern: /\\('|"){1}/g
+                    }
+                }
+            },
+            pattern: /(\(|\s|\[|\=|:|\+|\.|\{|,)(('|")([^\\\1]|\\.)*?(\3))/gm
+        },
+        {
+            name: 'comment',
+            pattern: /\/\*[\s\S]*?\*\/|(\/\/|\#)(?!.*('|").*?[^:](\/\/|\#)).*?$/gm
+        },
+        {
+            name: 'constant.numeric',
+            pattern: /\b(\d+(\.\d+)?(e(\+|\-)?\d+)?(f|d)?|0x[\da-f]+)\b/gi
+        },
+        {
+            matches: {
+                1: 'keyword'
+            },
+            pattern: /\b(and|array|as|b(ool(ean)?|reak)|c(ase|atch|har|lass|on(st|tinue))|d(ef|elete|o(uble)?)|e(cho|lse(if)?|xit|xtends|xcept)|f(inally|loat|or(each)?|unction)|global|if|import|int(eger)?|long|new|object|or|pr(int|ivate|otected)|public|return|self|st(ring|ruct|atic)|switch|th(en|is|row)|try|(un)?signed|var|void|while)(?=\b)/gi
+        },
+        {
+            name: 'constant.language',
+            pattern: /true|false|null/g
+        },
+        {
+            name: 'keyword.operator',
+            pattern: /\+|\!|\-|&(gt|lt|amp);|\||\*|\=/g
+        },
+        {
+            matches: {
+                1: 'function.call'
+            },
+            pattern: /(\w+?)(?=\()/g
+        },
+        {
+            matches: {
+                1: 'storage.function',
+                2: 'entity.name.function'
+            },
+            pattern: /(function)\s(.*?)(?=\()/g
+        }
+    ],
+    python: [
+        /**
+         * don't highlight self as a keyword
+         */
+        {
+            name: 'variable.self',
+            pattern: /self/g
+        },
+        {
+            name: 'constant.language',
+            pattern: /None|True|False|NotImplemented|\.\.\./g
+        },
+        {
+            name: 'support.object',
+            pattern: /object/g
+        },
+
+        /**
+         * built in python functions
+         *
+         * this entire list is 580 bytes minified / 379 bytes gzipped
+         *
+         * @see http://docs.python.org/library/functions.html
+         *
+         * @todo strip some out or consolidate the regexes with matching patterns?
+         */
+        {
+            name: 'support.function.python',
+            pattern: /\b(bs|divmod|input|open|staticmethod|all|enumerate|int|ord|str|any|eval|isinstance|pow|sum|basestring|execfile|issubclass|print|super|bin|file|iter|property|tuple|bool|filter|len|range|type|bytearray|float|list|raw_input|unichr|callable|format|locals|reduce|unicode|chr|frozenset|long|reload|vars|classmethod|getattr|map|repr|xrange|cmp|globals|max|reversed|zip|compile|hasattr|memoryview|round|__import__|complex|hash|min|set|apply|delattr|help|next|setattr|buffer|dict|hex|object|slice|coerce|dir|id|oct|sorted|intern)(?=\()/g
+        },
+        {
+            matches: {
+                1: 'keyword'
+            },
+            pattern: /\b(pass|lambda|with|is|not|in|from|elif|raise|del)(?=\b)/g
+        },
+        {
+            matches: {
+                1: 'storage.class',
+                2: 'entity.name.class',
+                3: 'entity.other.inherited-class'
+            },
+            pattern: /(class)\s+(\w+)\((\w+?)\)/g
+        },
+        {
+            matches: {
+                1: 'storage.function',
+                2: 'support.magic'
+            },
+            pattern: /(def)\s+(__\w+)(?=\()/g
+        },
+        {
+            name: 'support.magic',
+            pattern: /__(name)__/g
+        },
+        {
+            matches: {
+                1: 'keyword.control',
+                2: 'support.exception.type'
+            },
+            pattern: /(except) (\w+):/g
+        },
+        {
+            matches: {
+                1: 'storage.function',
+                2: 'entity.name.function'
+            },
+            pattern: /(def)\s+(\w+)(?=\()/g
+        },
+        {
+            name: 'entity.name.function.decorator',
+            pattern: /@([\w\.]+)/g
+        },
+        {
+            name: 'comment.docstring',
+            pattern: /('{3}|"{3})[\s\S]*?\1/gm
+        }
+    ]
+};
