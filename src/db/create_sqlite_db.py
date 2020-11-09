17238e6ef8107a16362866172e241ec49afde278
allEyezOnCode <alexandredewilde2001@gmail.com>
2020-11-09 15:29:37 +0100

    finish sqlite option

--- src/db/create_sqlite_db.py
+++ src/db/create_sqlite_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 if __name__ == "__main__":
-    conn = sqlite3.connect("documents.db", check_same_thread=False)
+    conn = sqlite3.connect("codewe.db", check_same_thread=False)
     c = conn.cursor()
     c.execute("CREATE TABLE documents (id INTEGER PRIMARY KEY AUTOINCREMENT, document_id, creation_date, last_viewed_date, content)")
     conn.commit()
