4fc43e503a6b29a8259d94ff0f3802d408a8b582
bhasher <bhasher.anonyme@gmail.com>
2020-11-12 00:25:48 +0100

    Download button

--- src/publics/js/editor/main.js
+++ src/publics/js/editor/main.js
@@ -2,7 +2,7 @@ import {PrismCustom} from "./prism/prism-custom.js";
 import {Socket} from "./socket.js";
 import {Editor} from "./editor.js";
 import Cursor from './cursor.js'
-
+import {download} from "./download.js";
 
 
 export const DEBUG = true;
@@ -14,3 +14,5 @@ const cursor = new Cursor(document.getElementById('editor'));
 for(const child of document.getElementById('editor').children){
     new PrismCustom(child, 'python').ApplyWithCaret();
 }
+
+download(document.getElementById('editor'), 'download');
