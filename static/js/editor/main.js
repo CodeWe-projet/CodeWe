ee5643be7c692b6ac83bb4221087809dc7b42010
bhasher <bhasher.anonyme@gmail.com>
2020-11-11 20:25:48 +0100

    Resolving major delete bug

--- static/js/editor/main.js
+++ static/js/editor/main.js
@@ -2,6 +2,8 @@ import {PrismCustom} from "./prism/prism-custom.js";
 import {Socket} from "./socket.js";
 import {Editor} from "./editor.js";
 
+
+
 export const DEBUG = true;
 
 const socket = new Socket(doc_id);
