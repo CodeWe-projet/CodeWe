d823dc3c93f9ef9d428a7b7b48bfe40406f34bc5
bhasher <bhasher.anonyme@gmail.com>
2020-11-14 02:12:30 +0100

    Muliline remove & text-alert when prevent default

--- src/publics/js/editor/text-alert.js
+++ src/publics/js/editor/text-alert.js
@@ -1,7 +1,8 @@
-function temporaryCardAlert(title, message, type, duration){
+export function temporaryCardAlert(title, message, duration, color='#1e90ff'){
     const id = getRandomString(11);
     const card = document.createElement('div');
     card.classList.add('alertCard');
+    card.style.backgroundColor = color;
     card.id = id;
     card.innerHTML = "<h3>" + title + "</h3><p>" + message + "</p>";
     document.getElementById('body').appendChild(card);
--- src/publics/js/utils.js
+++ src/publics/js/utils.js
@@ -69,6 +69,30 @@ export function getCaretCharacterOffsetWithin(element) {
     return caretOffset;
 }
 
+export function getCaretCharacterStartOffset(element) {
+    let caretOffset = 0;
+    let doc = element.ownerDocument || element.document;
+    let win = doc.defaultView || doc.parentWindow;
+    let sel;
+    if (typeof win.getSelection != "undefined") {
+        sel = win.getSelection();
+        if (sel.rangeCount > 0) {
+            let range = win.getSelection().getRangeAt(0);
+            let preCaretRange = range.cloneRange();
+            preCaretRange.selectNodeContents(element);
+            preCaretRange.setEnd(range.startContainer, range.startOffset);
+            caretOffset = preCaretRange.toString().length;
+        }
+    } else if ( (sel = doc.selection) && sel.type !== "Control") {
+        let textRange = sel.createRange();
+        let preCaretTextRange = doc.body.createTextRange();
+        preCaretTextRange.moveToElementText(element);
+        preCaretTextRange.setEndPoint("EndToEnd", textRange);
+        caretOffset = preCaretTextRange.text.length;
+    }
+    return caretOffset;
+}
+
 export function getCurrentElement(){
     return window.getSelection().getRangeAt(0).startContainer;
 }
