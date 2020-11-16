d621cfd84fc1121c12e76b8fadd1e6bf18c61b35
bhasher <bhasher.anonyme@gmail.com>
2020-11-16 23:23:12 +0100

    Footer welcome message

--- src/publics/js/dev/page/base.js
+++ src/publics/js/dev/page/base.js
@@ -12,14 +12,14 @@ import Socket from "/js/dev/utils/websocket/socket.js";
 // Display Welcome Message is new user
 new Welcome(_.id('welcome-button'));
 
+document.getElementById('link-welcome').onclick = Welcome.show;
+
+/*
 const socket = new Socket({
     secure: false,
     port: window.location.port,
     hostname: window.location.origin,
     pathname: 'ws',
-    /*params: {
-        room_id: 'e4D5f'
-    }*/
 });
 
 const a = {
@@ -31,4 +31,4 @@ const a = {
 socket.ws.onopen = e => {
     socket.send(a);
 }
-
+*/
